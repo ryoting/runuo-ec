d26c20576d238e4c4c5a71b225da9c05ce0455db
mutila@gmail.com <mutila@gmail.com@aad68551-ad90-291f-6384-78cc62229b2b>
2014-03-11 17:17:43 +0000

    Firts server and source version.

---
+++ trunk/Source/RunUO-3D/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RunUO_3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
